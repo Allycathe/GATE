c65f1f896274334aacb5732cc90b6ec9d68aff9b
MaiR42 <mair.escobar@mail.udp.cl>
2026-05-12 20:24:56 -0400

    Agregue configuracion de interfaz

--- gate/lib/config.dart
+++ gate/lib/config.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 
+// Configuracion de backend
+const String baseUrl = 'https://gate.blade.dedyn.io';
+
+
 // Configuraciones de interfaz
 
-const interfaceColor = Color.fromARGB(255, 102, 102, 255); // Default interfaceColor.fromARGB(255, 102, 102, 255)
-const textOptionColor = Color.fromARGB(255, 102, 102, 255); // Default textOptionColor.fromARGB(255, 102, 102, 255)
+// Default interfaceColor.fromARGB(255, 102, 102, 255)
+// Default textOptionColor.fromARGB(255, 102, 102, 255)
+
+const interfaceColor = Color.fromARGB(255, 255, 48, 58);
+const textOptionColor = Color.fromARGB(255, 255, 57, 57);
+
