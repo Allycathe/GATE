61d2d6f19ff2e2624abfcc58704babf74a302ae8
MaiR42 <mair.escobar@mail.udp.cl>
2026-05-28 19:29:22 -0400

    pre debug.dart

---
+++ gate/lib/pages/user_options.dart
@@ -0,0 +1,69 @@
+// lib/debug.dart
+import 'package:flutter/material.dart';
+import 'package:gate/custom_widgets/navbar.dart';
+import 'package:gate/custom_widgets/option_menu.dart';
+
+import '/config.dart';
+import 'login.dart';
+
+void logout(BuildContext context) {
+  // Limpiar sesión
+  userToken = "";
+
+  userId = 0;
+
+  userEmail = "";
+
+  userName = "";
+
+  userLastName = "";
+
+  userIsAdmin = false;
+
+  userSupermarketId = 0;
+
+  // Volver login
+  Navigator.pushAndRemoveUntil(
+    context,
+    MaterialPageRoute(
+      builder: (context) => const LoginPage(),
+    ),
+    (route) => false,
+  );
+}
+
+class UserOptions extends StatelessWidget {
+  const UserOptions({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppBar(),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 40,),
+            FilledButton(
+                onPressed: () {},
+                style: FilledButton.styleFrom(backgroundColor: interfaceColor),
+                child: const Text("Cambiar foto de perfil")),
+            SizedBox(
+              height: 20,
+            ),
+            FilledButton(
+              onPressed: () {
+                logout(context);
+              },
+              style: FilledButton.styleFrom(
+                backgroundColor: buttonColor,
+              ),
+              child: const Text("Cerrar sesión"),
+            ),
+            const Expanded(child: Text(" ")),
+            const OptionContainer()
+          ],
+        ),
+      ),
+    );
+  }
+}
