3b6aa482984853ba13a06ce6b9ec857fdca6058f
allycathe <alonso.iturra.j@gmail.com>
2026-06-08 18:19:48 -0400

    Cambios funcionales en el mapa

--- gate/ios/Flutter/flutter_export_environment.sh
+++ gate/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/hoqi/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/hoqi/GATE/gate"
+export "FLUTTER_ROOT=/home/blade/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/blade/Code/IS/GATE/gate"
+export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=/home/blade/Code/IS/GATE/gate/ios/Flutter/ephemeral/Packages/.packages/FlutterFramework"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
